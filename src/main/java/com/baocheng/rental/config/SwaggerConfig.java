71c1b6617631725135529a20cd375261f40bee07
bingyanlengyu <zhaojk@78.d1.01a8.ip4.static.sl-reverse.com>
2022-10-27 17:13:05 +0800

    swagger

---
+++ src/main/java/com/baocheng/rental/config/SwaggerConfig.java
@@ -0,0 +1,98 @@
+package com.baocheng.rental.config;
+
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.boot.autoconfigure.web.reactive.WebFluxAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Configuration // 标明是配置类
+@EnableSwagger2 //开启swagger功能
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html", "doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+        super.addResourceHandlers(registry);
+
+    }
+
+    @Bean
+    public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+        return new BeanPostProcessor() {
+            @Override
+            public Object postProcessAfterInitialization(Object bean, String beanName)throws BeansException {
+
+                if (bean instanceof WebMvcRequestHandlerProvider || bean instanceof WebFluxAutoConfiguration) {
+                    customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
+                }
+                return bean;
+            }
+
+            private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(List<T> mappings) {
+                List<T> copy = mappings.stream().filter(mapping -> mapping.getPatternParser() ==null)
+                        .collect(Collectors.toList());
+                 mappings.clear();
+                 mappings.addAll(copy);
+            }
+
+            @SuppressWarnings("unchecked")
+            private List getHandlerMappings(Object bean) {
+                try {
+                    Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
+                    field.setAccessible(true);
+                     return (List) field.get(bean);
+                 }catch (IllegalArgumentException | IllegalAccessException e) {
+                    throw new IllegalStateException(e);
+
+                 }
+
+            }
+
+        };
+
+    }
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)  // DocumentationType.SWAGGER_2 固定的，代表swagger2
+                .apiInfo(apiInfo()) // 用于生成API信息
+                .select() // select()函数返回一个ApiSelectorBuilder实例,用来控制接口被swagger做成文档
+                .apis(RequestHandlerSelectors.basePackage("com.baocheng.rental.controller")) // 用于指定扫描哪个包下的接口
+                .paths(PathSelectors.any())// 选择所有的API,如果你想只为部分API生成文档，可以配置这里
+                .build();
+    }
+
+    /**
+     * 用于定义API主界面的信息，比如可以声明所有的API的总标题、描述、版本
+     * @return
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("rental car API") //  可以用来自定义API的主标题
+                .description("SwaggerAPI") // 可以用来描述整体的API
+                .termsOfServiceUrl("") // 用于定义服务的域名
+                .version("1.0") // 可以用来定义版本。
+                .build(); //
+    }
+}
